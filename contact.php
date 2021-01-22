a5cfd8d2df3d5d90064da0f72b4f1afe115fa43a
Elogeek <elodiechristin@gmail.com>
2021-01-22 16:56:14 +0100

    go

---
+++ contact.php
@@ -0,0 +1,27 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>contact</title>
+</head>
+<body>
+<div>
+    <form method="get" action="coucou.php">
+        <div>
+            <label for="nom">Nom</label>
+            <input type="text" name="nom" id="nom">
+        </div>
+        <div>
+            <label for="mail">Mail</label>
+            <input type="email" name="mail" id="mail">
+        </div>
+        <input type="submit" value="Envoyer">
+    </form>
+</div>
+</body>
+</html>
